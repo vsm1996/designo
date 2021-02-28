bf9f060b7530511552b84f7ce58a091c0f8f45ca
Vanessa Martin <vanessa.s.martin96@gmail.com>
2021-02-27 17:18:08 -0800

    home tablet view complete

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -37,21 +37,27 @@ const Home = () => {
       <section className="home-values__container">
         <div className="home-values__card">
           <img src={CircleSVG} alt="" className="circle-first" />
-          <PassionateSVG />
-          <h3 className="home-values__card-title">Passionate</h3>
-          <p className="home-values__card-text"> Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions. </p>
+          <span className="home-values__card-svg"> <PassionateSVG /> </span>
+          <div className="home-values__card-text-content">
+            <h3 className="home-values__card-title">Passionate</h3>
+            <p className="home-values__card-text"> Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions. </p>
+          </div>
         </div>
         <div className="home-values__card m-lr-card">
           <img src={CircleSVG} alt="" className="circle-second" />
-          <ResourcefulSVG />
-          <h3 className="home-values__card-title">Resourceful</h3>
-          <p className="home-values__card-text"> Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients’ needs. </p>
+          <span className="home-values__card-svg"> <ResourcefulSVG /> </span>
+          <div className="home-values__card-text-content">
+            <h3 className="home-values__card-title">Resourceful</h3>
+            <p className="home-values__card-text"> Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients’ needs. </p>
+          </div>
         </div>
         <div className="home-values__card">
           <img src={CircleSVG} alt="" className="circle-third" />
-          <FriendlySVG />
-          <h3 className="home-values__card-title">Friendly</h3>
-          <p className="home-values__card-text"> We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide. </p>
+          <span className="home-values__card-svg"> <FriendlySVG /> </span>
+          <div className="home-values__card-text-content">
+            <h3 className="home-values__card-title">Friendly</h3>
+            <p className="home-values__card-text"> We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide. </p>
+          </div>
         </div>
       </section>
     </div>
